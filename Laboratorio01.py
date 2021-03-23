ab40cc0fd2a9d351cacb909946625f6add5b0acc
CristianConcha <ciconcha@miuandes.cl>
2021-03-22 22:45:15 -0300

    Empezar a arreglar el codigo

--- Laboratorio01.py
+++ Laboratorio01.py
@@ -1,130 +1,128 @@
 from numpy import random
-<<<<<<< HEAD
+
 import re
 
 def Board(tablero): #Imprime tablero
     for row in range(len(tablero)):
-        #for column in range(len(Coordinate_Column)):
         print(tablero[row])
+    
 
 
-def Number_Board():
-    coordenada = input("Enter a coordinate:")
-    d = re.sub(r"[^\w\s]","", coordenada)
-    print(d[0]) #Cambiar coordenadas
-    print(d[1])
-    x = int(d[0])
-    y = int(d[1])
-    tablero = Coordinate_Row
+def Number_Board(tablero, x, y):
+
 
     tablero[x].pop(y)
     tablero[x].insert(y, Number_Row[x][y])
+    card = Number_Row[x][y]
     Board(tablero)
-    """
-    tablerofi = []
-    for row in range(len(Coordinate_Row)):
-        tablerocol = []
-        for column in range(len(Coordinate_Column)):
-            if row == int(x[0]) and column == int(x[1]):
-                tablerocol.append(Number_Row[row][column])
+    return card
 
-            else:
-                tablerocol.append(Coordinate_Row[row][column])
-        print(tablerocol)
-    """
-=======
->>>>>>> 1f0281e609c795a61ac76c1ef13d489c10ab55ce
+
+def Condition(card1, card2, tablero, x, y): 
+
+    if card1 == card2:
+        Numbers.remove(card1)
+        tablero[x].pop(y)
+        tablero[x].insert(y, "      ")
+        
+    
+    else:
+        tablero[x].pop(y)
+        tablero[x].insert(y, ("(%d, %d)" % (x, y)))
+
+        print()
+        print(Coordinate_Row[x][y])
+        print()
+ 
+
+    
+    return tablero
+    
 
 Number_cards = 8 #int(input("How many cards do you want to play: "))
 Pair_of_cards = Number_cards * 2
 
 
-<<<<<<< HEAD
-Coordinate_Row = []  
+ 
 Numbers = []  
 Number_Row = []
+Coordinate_Row = []
 
-for row in range(4):
-    Coordinate_Column = []
+for row in range(4): ## Bien
     Column_Number = []
+    Coordinate_Column = []
 
     while len(Column_Number) <= 3:
         card = random.randint(1, 9)
 
         if Numbers.count(card) < 2:
-            Coordinate_Column.append("(%d, %d)" % (row, len(Column_Number)))
-
             Numbers.append(card)
             Column_Number.append(card)
+            Coordinate_Column.append("(%d, %d)" % (row, len(Column_Number)-1))
         else:
             continue
 
     Number_Row.append(Column_Number)
     Coordinate_Row.append(Coordinate_Column)
-    
-    print(Coordinate_Column, Column_Number)
 
-print()
+    print(Column_Number)  ### Borrar
+print(Coordinate_Row[3][3])
+
 
 Player_1 = 0
 Player_2 = 0
-x = 0
-while x == 0:  # len(Numbers)== 0
-    if x == 0:
-        print("It´s player 1 turn:  \n")
-        Board(Coordinate_Row)
-        Number_Board()
+x = 1
+d = 0
+tablero = Coordinate_Row
 
-        x += 1
-    """
-    elif x == 1:
-        print("It´s player 2 turn:  \n")
-        board()
-        x -= 1
-    """
+while x <= 2:  # len(Numbers)== 0
     
-
-
-
-
-
-=======
-Fila = [] #coordenadas
-coordenada_x = 0
-for fila in range(4):
-    Columna = []
-    coordenada_y = 0
-    for columna in range(4):
-        Columna.append("(%d, %d)" %(coordenada_x, coordenada_y))
-        coordenada_y += 1
-    Fila.append(Columna)
-    coordenada_x += 1
+    print("It´s player %d turn:  \n" % (x))
+    Board(tablero)
     
-    print(Columna)
+        
+    coordenada = input("Enter a coordinate:")
+    d = re.sub(r"[^\w\s]", "", coordenada) 
+    print()
+    print(d[0])  # Cambiar coordenadas
+    print(d[1])
+    x1 = int(d[0])
+    y1 = int(d[1])
 
-print(Fila[3][3])
-print()
+    card1 = (Number_Board(tablero, x1, y1))
+    print()
+    print(card1)
 
-numeros =[] #numeros aleatorios
-Filanum = []
 
-while len(Filanum) <= 4:
-    Columnanum = []
+    coordenada = input("Enter a coordinate:")
+    d = re.sub(r"[^\w\s]", "", coordenada)
+    print()
+    print(d[0])  # Cambiar coordenadas
+    print(d[1])
+    x2 = int(d[0])
+    y2 = int(d[1])
+    card2 = (Number_Board(tablero, x2, y2))
+    print()
+    print(card2)
 
-    while len(Columnanum) <= 4:
-        card = random.randint(1, 9)
-        if numeros.count(card) < 2:
-            numeros.append(card)
-            Columnanum.append(card)
+    Condition(card1, card2, tablero, x1, y1)
+    Condition(card1, card2, tablero, x2, y2)
+    Board(tablero)
+
+    if x == 1:
+        if card1 == card2:
+            Player_1 += 1
         else:
-            continue
-    Filanum.append(Columnanum)
+            x += 1
+    elif x == 2:
+        if card1 == card2:
+            Player_2 += 1
+        else:
+            x -=1
+  
 
-    print(Columnanum)
-print(numeros.count(3))
-print("jedjvciecjoiew")
-print(Filanum[3][3])
->>>>>>> 1f0281e609c795a61ac76c1ef13d489c10ab55ce
+
+
 
 
 
